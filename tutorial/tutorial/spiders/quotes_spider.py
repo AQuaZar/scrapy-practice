d369bc77eda2ac748f1d276b5e4a3b620d0bafae
AQuaZar <greenskullcompany@gmail.com>
2020-04-27 17:08:23 +0300

    Add recursive crawling through all pages

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -3,10 +3,7 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-    start_urls = [
-        "http://quotes.toscrape.com/page/1/",
-        "http://quotes.toscrape.com/page/2/",
-    ]
+    start_urls = ["http://quotes.toscrape.com/page/1/"]
 
     def parse(self, response):
         for quote in response.css("div.quote"):
@@ -15,3 +12,8 @@ class QuotesSpider(scrapy.Spider):
                 "author": quote.css("small.author::text").get(),
                 "tags": quote.css("div.tags a.tag::text").getall(),
             }
+
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
